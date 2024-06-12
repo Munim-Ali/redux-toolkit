ceddf3f11773a94b0f5821daf00b62d453f4c8ac
Munim-Ali <munimali52@gmail.com>
2024-06-12 16:03:28 +0500

    first commit

---
+++ reduxToolkitTodo/src/app/store.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducers from "../features/todo/todoSlice";
+
+export const store = configureStore({
+  reducer: todoReducers,
+});
